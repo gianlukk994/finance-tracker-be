19b82413538bf54e7f9f27ac99f8b98bb482ff66
Gianluca <eliagianluca94@gmail.com>
2020-06-22 14:58:22 +0200

    Add frontend actions for portfolio

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,7 @@ class Stock < ApplicationRecord
   has_many :users, through: :user_stocks
 
   validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
@@ -12,4 +13,9 @@ class Stock < ApplicationRecord
       return nil
     end
   end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
 end
